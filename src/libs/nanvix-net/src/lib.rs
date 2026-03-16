145bb5835ec50edfbd91105bd711835b1a982f2c
Antonio Drumond <adcdstom@gmail.com>
2026-03-16 19:25:03 -0300

    Init nanvix-net + Time to Instant function

---
+++ src/libs/nanvix-net/src/lib.rs
@@ -0,0 +1,26 @@
+use sys::error::Error;
+use syscall::safe::time::Time;
+use smoltcp::{
+    phy::{Device, DeviceCapabilities, Medium, RxToken, TxToken},
+    iface::{Config, Interface, SocketSet},
+    wire::{EthernetAddress, IpAddress, IpCidr, Ipv4Address},
+    socket::tcp,
+    time::Instant,
+};
+
+// Not sure, got from Valedo's tcp.rs file...
+const IP: &str = "10.0.2.15"; // QEMU user networking default IP
+const GATEWAY: &str = "10.0.2.2"; // QEMU user networking gateway
+const PORT: u16 = 5555;
+
+pub fn now_instant() -> Result<Instant, Error> {
+    let now = Time::now()?;
+    let millis = now.seconds() * 1000 + (now.nanoseconds() as u64 / 1_000_000);
+    Ok(Instant::from_millis(millis as i64))
+}
+
+/*
+#[cfg(test)]
+mod tests {
+}
+*/
