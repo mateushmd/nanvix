94abd2da391aba34948eee29bffb7f60bff89f39
Antonio Drumond <adcdstom@gmail.com>
2026-04-06 15:46:50 -0300

    driver somewehat half done

--- src/libs/nanvix-net/src/lib.rs
+++ src/libs/nanvix-net/src/lib.rs
@@ -1,28 +1,31 @@
+#![no_std]
+
 extern crate alloc;
 
-use sys::error::Error;
-use syscall::safe::time::Time;
-use smoltcp::{
-    phy::{Device, DeviceCapabilities, Medium, RxToken, TxToken},
-    iface::{Config, Interface, SocketSet},
-    wire::{EthernetAddress, IpAddress, IpCidr, Ipv4Address},
-    socket::tcp,
-    time::Instant,
+#[cfg(test)]
+extern crate std;
+
+pub mod e1000;
+
+#[cfg(feature = "syscall-time")]
+use ::smoltcp::time::Instant;
+#[cfg(feature = "syscall-time")]
+use ::sys::error::Error;
+#[cfg(feature = "syscall-time")]
+use ::syscall::safe::time::Time;
+
+pub use self::e1000::{
+    E1000Device,
+    E1000RxToken,
+    E1000TxToken,
+    KernelFunctions,
+    RxDesc,
+    TxDesc,
 };
 
-// Not sure, got from Valedo's tcp.rs file...
-const IP: &str = "10.0.2.15"; // QEMU user networking default IP
-const GATEWAY: &str = "10.0.2.2"; // QEMU user networking gateway
-const PORT: u16 = 5555;
-
+#[cfg(feature = "syscall-time")]
 pub fn now_instant() -> Result<Instant, Error> {
     let now = Time::now()?;
     let millis = now.seconds() * 1000 + (now.nanoseconds() as u64 / 1_000_000);
     Ok(Instant::from_millis(millis as i64))
 }
-
-/*
-#[cfg(test)]
-mod tests {
-}
-*/
