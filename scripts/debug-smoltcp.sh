4846573a282cee4274d0626892210bb76ac009ee
Antonio Drumond <adcdstom@gmail.com>
2026-05-22 18:47:29 -0300

    [netd] W: Fix finding MAC addr + init tx

--- scripts/debug-smoltcp.sh
+++ scripts/debug-smoltcp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-make debug TARGET=x86 MACHINE=qemu-pc IMAGE=build/iso/nanvix.iso LOG_LEVEL=trace GRUB_CFG_SCRIPT=build/iso/boot/grub/grub-net.cfg
+make debug TARGET=x86 MACHINE=qemu-pc IMAGE=build/iso/nanvix.iso LOG_LEVEL=debug GRUB_CFG_SCRIPT=build/iso/boot/grub/grub-net.cfg
